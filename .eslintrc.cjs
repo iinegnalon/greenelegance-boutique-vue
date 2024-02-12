ff1188c861085eb7474d5dde53894658c6a2fbb2
iinegnalon <iinegnalon@gmail.com>
2024-02-12 13:58:19 +0400

    Eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,12 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // disable multi word rule
     'vue/multi-word-component-names': 0,
+    // disable line separator check
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
   }
 }
